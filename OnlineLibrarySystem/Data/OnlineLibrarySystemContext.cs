17fee579d7fbbe94daa7a91a3d716c13a065af9b
SoumyaMyaka <soumyareddy1008@gmail.com>
2025-06-12 10:28:53 +0530

    CapstoneProject(online-library-system)

---
+++ OnlineLibrarySystem/Data/OnlineLibrarySystemContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OnlineLibrarySystem.Models;
+
+namespace OnlineLibrarySystem.Data
+{
+    public class OnlineLibrarySystemContext : DbContext
+    {
+        public OnlineLibrarySystemContext (DbContextOptions<OnlineLibrarySystemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<OnlineLibrarySystem.Models.Book> Book { get; set; } = default!;
+    }
+}
